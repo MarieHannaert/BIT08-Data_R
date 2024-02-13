fea0d344fcddc2f5d106328f6e8494d31055373a
Hannaert Marie <marie.hannaert@student.howest.be>
2024-02-13 16:26:08 +0100

    adding first exercise

---
+++ exercise_1.R
@@ -0,0 +1,12 @@
+#atomic vector number 
+avchr <- c("one","two", "three")
+avint <- as.integer(c(1,2,3))
+avlog <- c(TRUE,FALSE,TRUE,FALSE)
+typeof(avchr)
+typeof(avint)
+typeof(avlog)
+class(avchr)
+class(avint)
+class(avlog)
+mtrx <- matrix(1:12, nrow=4, ncol=3)
+
