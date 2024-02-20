44072619cf3357be2a43c52bfe67d276c47c71c4
Hannaert Marie <marie.hannaert@student.howest.be>
2024-02-20 15:15:06 +0100

    adding an update of the exercise

--- exercise1.5.R
+++ exercise1.5.R
@@ -11,3 +11,31 @@ write.xlsx(x = ChickWeight,
            row.names = FALSE)
 ChickWeight$Day <- paste0("day ",ChickWeight$Time)
 sub("d", "D", ChickWeight)
+
+grepl("day 0", ChickWeight$Day)
+class(ChickWeight$Day)
+ChickWeight$Day <- as.factor(ChickWeight$Day)
+class(ChickWeight$Day)
+
+class(ChickWeight$Chick)
+unique(sort(ChickWeight$Chick, decreasing = TRUE))
+#Chick2 <- order(sort(ChickWeight$Chick))
+Chick2 <- as.numeric(ChickWeight$Chick)
+
+ChickWeight$Chick2 <- Chick2
+
+
+sort(ChickWeight$Chick2, decreasing=TRUE)
+ChickWeight[ChickWeight$Diet=="1", ]
+
+ChickWeight1 <- ChickWeight[which(ChickWeight$Diet=="1")]
+ChickWeight1subset <- subset(ChickWeight, ChickWeight$Diet=="1")
+
+as.unique(ChickWeight$Diet)
+subsets <- c("1", "2", "3", "4")
+listofdfs <- list() #creation of an emptylist
+for(subset in subsets){
+  dataframe <- paste0("ChickWeight",subset)
+  print(paste0("Dataframe = ", dataframe))
+  listofdfs[[dataframe]] <- ChickWeight[which(ChickWeight$Diet==subset),]
+}
